b826622272464f9c98c76b93f5afdb76f8799d01
Гуртовая Анна <103031042+aV0kaDKA@users.noreply.github.com>
2022-09-20 01:18:39 +0300

    Delete TestAuthor.java

--- TestAuthor.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.kvbo1.gurtovaya;
-
-public class TestAuthor {
-    public static void main(String[] args) {
-        Author a = new Author("Анна", "gurtovaya@aga.com", 'Ж');
-        System.out.println(a);
-    }
-}
